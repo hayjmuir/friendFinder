a596f509c24d2017f013c0ab990a94732a8006c4
hayjmuir <haymuir@gmail.com>
2020-05-02 13:04:41 -0700

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
-        name: "",
-        photo: "",
+        name: "Harold",
+        photo: "https://imgur.com/t/hide_the_pain_harold/BRd6X7D",
         scores: [
             "5",
             "5",
@@ -15,8 +15,8 @@ var friends = [{
         ]
     },
     {
-        name: "",
-        photo: "",
+        name: "Joe",
+        photo: "https://en.wikipedia.org/wiki/Joe_Exotic#/media/File:Joe_Exotic_(Santa_Rose_County_Jail).png",
         scores: [
             "4",
             "4",
@@ -31,8 +31,8 @@ var friends = [{
         ]
     },
     {
-        name: "",
-        photo: "",
+        name: "Karl",
+        photo: "https://en.wikipedia.org/wiki/Karl_Marx#/media/File:Karl_Marx_001.jpg",
         scores: [
             "3",
             "3",
@@ -47,8 +47,8 @@ var friends = [{
         ]
     },
     {
-        name: "",
-        photo: "",
+        name: "Vicky",
+        photo: "https://en.wikipedia.org/wiki/Queen_Victoria#/media/File:Queen_Victoria_by_Bassano.jpg",
         scores: [
             "2",
             "2",
@@ -63,8 +63,8 @@ var friends = [{
         ]
     },
     {
-        name: "",
-        photo: "",
+        name: "Tommy",
+        photo: "https://en.wikipedia.org/wiki/Tom_Jones_(singer)#/media/File:Tom_Jones_Hampton_Court_Palace_2007.jpg",
         scores: [
             "1",
             "1",
